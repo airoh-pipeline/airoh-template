30213d2c6f823dd7c9b67d96e6558380e61a815a
Luna Bellec <pierre.bellec@gmail.com>
2025-06-07 19:43:25 -0400

    Adding parsing of downloaded assets.

--- tasks.py
+++ tasks.py
@@ -35,11 +35,23 @@ def run_figure(c):
     Generate figures from the simulation output using a notebook.
     """
     from airoh.utils import run_figures, ensure_dir_exist
+
     notebooks_dir = Path(c.config.get("notebooks_dir"))
     output_dir = Path(c.config.get("output_data_dir")).resolve()
-    env = {**os.environ, "OUTPUT_DIR": str(output_dir)}
+    source_dir = Path(c.config.get("source_data_dir")).resolve()
+
+    env = {
+        **os.environ,
+        "OUTPUT_DIR": str(output_dir),
+        "SOURCE_DATA_DIR": str(source_dir),
+    }
+
     ensure_dir_exist(c, "output_data_dir")
     run_figures(c, notebooks_dir, output_dir, env=env)
+
+@task(pre=[run_simulation, run_figure])
+def run(c):
+    print("all analyses completed")
 
 @task
 def clean(c):
@@ -47,5 +59,6 @@ def clean(c):
     Clean the output folder.
     """
     from airoh.utils import clean_folder
-    clean_folder(c, "output_data_dir")
+    clean_folder(c, "output_data_dir", "*.png")
+    clean_folder(c, "output_data_dir", "*.csv")
 
